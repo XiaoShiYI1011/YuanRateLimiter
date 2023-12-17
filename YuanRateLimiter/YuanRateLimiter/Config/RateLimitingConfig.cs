5d0dfe52d55fa3fa6bf19c28c36bf5fe738a2e19
十一 <xiaoshiyi1011@163.com>
2023-12-17 23:43:14 +0800

    重构限流配置类，为集成漏桶限流算法做准备

--- YuanRateLimiter/YuanRateLimiter/Config/RateLimitingConfig.cs
+++ YuanRateLimiter/YuanRateLimiter/Config/RateLimitingConfig.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using YuanRateLimiter.Enum;
+
+/*
  * 类名：RateLimitingConfig
  * 描述：限流配置类
  * 创 建 者：十一 
@@ -9,19 +11,45 @@ namespace YuanRateLimiter.Config
     public class RateLimitingConfig
     {
         public bool EnableRateLimiting { get; set; }
+        public int HttpStatusCode { get; set; } = 429;
+        public string LimitingMessage { get; set; } = "Request restricted, you are accessing too frequently!";
+        public string CacheKey { get; set; }
+        public RateLimiterModel RateLimiterModel { get; set; }
+        public Ratelimitingrule RateLimitingRule { get; set; }
+    }
 
-        public int HttpStatusCode { get; set; }
-
-        public string? CacheKey {  get; set; }
-
+    public class Ratelimitingrule
+    {
         public bool IsAllApiRateLimiting { get; set; }
-
-        public IsAllApiFlowLimitingRule IsAllApiFlowLimitingRule { get; set; }
-
+        public IsAllapiflowlimitingrule IsAllApiFlowLimitingRule { get; set; }
         public string RateLimitingLogLevel { get; set; }
+        public Methodflowlimitingrule[] MethodFlowLimitingRules { get; set; }
+        public Apiflowlimitingrule[] ApiFlowLimitingRules { get; set; }
+    }
 
-        public List<MethodFlowLimitingRule> MethodFlowLimitingRules { get; set; }
+    public class IsAllapiflowlimitingrule
+    {
+        public int Capacity { get; set; }
+        public int TokensPerSecond { get; set; }
+        public int MaxRequests { get; set; }
+        public int RateLimit { get; set; }
+    }
 
-        public List<ApiFlowLimitingRule> ApiFlowLimitingRules { get; set; }
+    public class Methodflowlimitingrule
+    {
+        public string Method { get; set; }
+        public int Capacity { get; set; }
+        public int TokensPerSecond { get; set; }
+        public int MaxRequests { get; set; }
+        public int RateLimit { get; set; }
+    }
+
+    public class Apiflowlimitingrule
+    {
+        public string Path { get; set; }
+        public int Capacity { get; set; }
+        public int TokensPerSecond { get; set; }
+        public int MaxRequests { get; set; }
+        public int RateLimit { get; set; }
     }
 }
