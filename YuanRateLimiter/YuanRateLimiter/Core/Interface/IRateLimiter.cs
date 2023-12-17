c9e9fa8e1a1eb4021e6287723903f1fa7db3d767
十一 <xiaoshiyi1011@163.com>
2023-12-17 23:40:37 +0800

    新增限流算法接口

---
+++ YuanRateLimiter/YuanRateLimiter/Core/Interface/IRateLimiter.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * 接 口 名：IRateLimiter
+ * 描述：限流算法接口
+ * 创 建 者：十一
+ * 创建时间：2023/12/17 20:19:31 
+ */
+using Microsoft.AspNetCore.Http;
+
+namespace YuanRateLimiter.Core.Interface
+{
+    public interface IRateLimiter
+    {
+        /// <summary>
+        /// 检查限流
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        Task<bool> CheckRateLimit(HttpContext context);
+    }
+}
