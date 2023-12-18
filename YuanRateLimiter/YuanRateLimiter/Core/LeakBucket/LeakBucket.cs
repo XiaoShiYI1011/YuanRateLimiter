ad3caf061b32850a67f33ec78192455bd5b9f06c
十一 <xiaoshiyi1011@163.com>
2023-12-18 18:59:42 +0800

    添加漏桶限流、滑动窗口限流算法，但方法未实现

---
+++ YuanRateLimiter/YuanRateLimiter/Core/LeakBucket/LeakBucket.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using YuanRateLimiter.Core.Interface;
+
+/*
+ * 类名：LeakBucket
+ * 描述：漏桶算法
+ * 创 建 者：十一 
+ * 创建时间：2023/12/18 0:08:44 
+ */
+namespace YuanRateLimiter.Core.LeakBucket
+{
+    internal class LeakBucket : IRateLimiter
+    {
+        public async Task<bool> CheckRateLimit(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YuanRateLimiter/YuanRateLimiter/Core/SlidingWindow/SlidingWindow.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using YuanRateLimiter.Core.Interface;
+
+/*
+ * 类名：SlidingWindow
+ * 描述：
+ * 创 建 者：十一 
+ * 创建时间：2023/12/18 18:56:39 
+ */
+namespace YuanRateLimiter.Core.SlidingWindow
+{
+    public class SlidingWindow : IRateLimiter
+    {
+        public async Task<bool> CheckRateLimit(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
