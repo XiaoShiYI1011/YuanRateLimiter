811777a9f90cebeb6055f73bf8399979f46a7453
十一 <xiaoshiyi1011@163.com>
2023-12-27 16:13:34 +0800

    部分方法使用低版本语法重构

--- YuanRateLimiter/YuanRateLimiter/Cache/MemoryCacheRepository.cs
+++ YuanRateLimiter/YuanRateLimiter/Cache/MemoryCacheRepository.cs
@@ -13,14 +13,11 @@ namespace YuanRateLimiter.Cache
     /// <summary>
     /// MemoryCache 仓储类
     /// </summary>
-    public class MemoryCacheRepository : ICacheService
+    internal class MemoryCacheRepository : ICacheService
     {
         private readonly MemoryCache memoryCache;
 
-        public MemoryCacheRepository(MemoryCache memoryCache)
-        {
-            this.memoryCache = memoryCache;
-        }
+        public MemoryCacheRepository(MemoryCache memoryCache) => this.memoryCache = memoryCache;
 
         /// <summary>
         /// 添加一条缓存数据
@@ -29,10 +26,7 @@ namespace YuanRateLimiter.Cache
         /// <param name="key">Key</param>
         /// <param name="value">Value</param>
         /// <returns></returns>
-        public bool Set<T>(string key, T value)
-        {
-            throw new NotImplementedException();
-        }
+        public bool Set<T>(string key, T value) => this.memoryCache.Set(key, value);
 
         /// <summary>
         /// 添加一条缓存数据，可设置过期时间
@@ -42,10 +36,7 @@ namespace YuanRateLimiter.Cache
         /// <param name="value">Value</param>
         /// <param name="expires">过期时间</param>
         /// <returns></returns>
-        public bool Set<T>(string key, T value, TimeSpan expire)
-        {
-            throw new NotImplementedException();
-        }
+        public bool Set<T>(string key, T value, TimeSpan expire) => this.memoryCache.Set(key, value, expire);
 
         /// <summary>
         /// 添加一条数据到List
@@ -55,7 +46,9 @@ namespace YuanRateLimiter.Cache
         /// <param name="value">Value</param>
         public void ListAdd<T>(string key, T value)
         {
-            throw new NotImplementedException();
+            var data = Get<List<T>>(key) ?? new List<T>();
+            data.Add(value);
+            this.memoryCache.Set(key, data);
         }
 
         /// <summary>
@@ -67,7 +60,15 @@ namespace YuanRateLimiter.Cache
         /// <returns></returns>
         public int ListLeftPush<T>(string key, IEnumerable<T> values)
         {
-            throw new NotImplementedException();
+            var data = Get<List<T>>(key) ?? new List<T>();
+            int count = 0;
+            foreach (var value in values)
+            {
+                count++;
+                data.Insert(0, value);
+            }
+            Set(key, data);
+            return count;
         }
 
         /// <summary>
@@ -79,17 +80,22 @@ namespace YuanRateLimiter.Cache
         /// <returns></returns>
         public int ListRightPush<T>(string key, IEnumerable<T> values)
         {
-            throw new NotImplementedException();
+            var data = Get<List<T>>(key) ?? new List<T>();
+            int count = 0;
+            foreach (var value in values)
+            {
+                count++;
+                data.Add(value);
+            }
+            Set(key, data);
+            return count;
         }
 
         /// <summary>
         /// 根据 Key 删除缓存数据
         /// </summary>
         /// <param name="key">Key</param>
-        public void DelKey(string key)
-        {
-            throw new NotImplementedException();
-        }
+        public void DelKey(string key) => this.memoryCache.Remove(key);
 
         /// <summary>
         /// List（头）左删，返回最左边一个元素
@@ -99,7 +105,15 @@ namespace YuanRateLimiter.Cache
         /// <returns></returns>
         public T ListLeftPop<T>(string key)
         {
-            throw new NotImplementedException();
+            var list = Get<List<T>>(key);
+            if (list != null && list.Count > 0)
+            {
+                var first = list[0];
+                list.RemoveAt(0);
+                Set(key, list);
+                return first;
+            }
+            return default;
         }
 
         /// <summary>
@@ -110,7 +124,15 @@ namespace YuanRateLimiter.Cache
         /// <returns></returns>
         public T ListRightPop<T>(string key)
         {
-            throw new NotImplementedException();
+            var list = Get<List<T>>(key);
+            if (list != null && list.Count > 0)
+            {
+                var last = list[list.Count - 1];
+                list.RemoveAt(list.Count - 1);
+                Set(key, list);
+                return last;
+            }
+            return default;
         }
 
         /// <summary>
@@ -119,10 +141,7 @@ namespace YuanRateLimiter.Cache
         /// <typeparam name="T">序列化类型</typeparam>
         /// <param name="key">Key</param>
         /// <returns></returns>
-        public T Get<T>(string key)
-        {
-            throw new NotImplementedException();
-        }
+        public T Get<T>(string key) => this.memoryCache.Get<T>(key);
 
         /// <summary>
         /// 获取List
@@ -132,7 +151,9 @@ namespace YuanRateLimiter.Cache
         /// <returns></returns>
         public List<T> ListGetAll<T>(string key)
         {
-            throw new NotImplementedException();
+            var data = this.memoryCache.Get<List<T>>(key);
+            if (data == null) return new List<T>();
+            return data;
         }
 
         /// <summary>
@@ -141,10 +162,7 @@ namespace YuanRateLimiter.Cache
         /// <param name="key">Key</param>
         /// <param name="value">变化量</param>
         /// <returns></returns>
-        public double Decrement(string key, double value)
-        {
-            throw new NotImplementedException();
-        }
+        public double Decrement(string key, double value) => this.memoryCache.Decrement(key, value);
 
         /// <summary>
         /// 递增，原子操作，乘以100后按整数操作
@@ -152,20 +170,14 @@ namespace YuanRateLimiter.Cache
         /// <param name="key">Key</param>
         /// <param name="value">变化量</param>
         /// <returns></returns>
-        public double Increment(string key, double value)
-        {
-            throw new NotImplementedException();
-        }
+        public double Increment(string key, double value) => this.memoryCache.Increment(key, value);
 
         /// <summary>
         /// 缓存 Key 是否存在
         /// </summary>
         /// <param name="key">Key</param>
         /// <returns></returns>
-        public bool ExistsKey(string key)
-        {
-            throw new NotImplementedException();
-        }
+        public bool ExistsKey(string key) => this.memoryCache.ContainsKey(key);
 
         /// <summary>
         /// 设置缓存Key的过期时间
@@ -173,9 +185,6 @@ namespace YuanRateLimiter.Cache
         /// <param name="key">Key</param>
         /// <param name="expire">过期时间</param>
         /// <returns></returns>
-        public bool SetExpires(string key, TimeSpan expire)
-        {
-            throw new NotImplementedException();
-        }
+        public bool SetExpires(string key, TimeSpan expire) => this.memoryCache.SetExpire(key, expire);
     }
 }
