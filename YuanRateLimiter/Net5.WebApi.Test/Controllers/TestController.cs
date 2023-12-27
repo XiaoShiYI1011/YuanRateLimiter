37a26e3a83b12f29a791f16587fe4d716cf36f76
十一 <xiaoshiyi1011@163.com>
2023-12-27 18:28:11 +0800

    新增Net5测试项目

---
+++ YuanRateLimiter/Net5.WebApi.Test/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Net5.WebApi.Test.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> Test01() => await Task.FromResult("api/Test/Test01");
+
+        [HttpPost]
+        public async Task<string> Test02() => await Task.FromResult("api/Test/Test02");
+
+        [HttpPut]
+        public async Task<string> Test03() => await Task.FromResult("api/Test/Test03");
+
+        [HttpDelete]
+        public async Task<string> Test04() => await Task.FromResult("api/Test/Test04");
+    }
+}
